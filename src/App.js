7bab770a625182b3b20f239a31c4edc9c8f1ce91
Shubhangi Shinde <158753518+shubhangi-shinde21595@users.noreply.github.com>
2024-06-15 12:32:21 +0530

    switching component for displaying different page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link exact to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link exact to="/about">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link exact to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
